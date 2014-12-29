1584f536174c50be653e562f2ba6ef97cd7b937a
John Unruh <j_unruh@hotmail.com>
2014-12-28 17:15:03 -0700

    change for new google re-captcha API

--- src/clj_recaptcha/client.clj
+++ src/clj_recaptcha/client.clj
@@ -1,104 +1,68 @@
 (ns clj-recaptcha.client
   (:require [clj-http.client :as client]
+            [clojure.data.json :as json]
             [clj-http.conn-mgr :as manager]))
 
-(def http-api  "http://www.google.com/recaptcha/api")
 (def https-api "https://www.google.com/recaptcha/api")
 
 (defn- parse-response
   [^String response]
   (if-not (nil? response)
-    (let [[valid error] (clojure.string/split-lines response)
-          valid         (= "true" valid)]
-      {:valid? valid :error error})
-    {:valid? false :error "incorrect-captcha-sol"}))
+    (let [return-value (clojure.data.json/read-str response :key-fn keyword)
+          valid (:success return-value)
+          error (:error-codes return-value)]
+      {:valid? valid :error error})))
+
 
 (defn verify
   "Verifies a user's answer for a reCAPTCHA challenge.
 
    private-key - your private key
-   challenge   - the value of recaptcha_challenge_field sent via the form
    response    - the value of recaptcha_response_field sent via the form
-   remote-ip   - the IP address of the user who solved the CAPTCHA
 
    Optional parameters:
-      :ssl?               - use HTTPS or HTTP? (default false)
       :proxy-host         - a proxy host
       :proxy-port         - a proxy port
-      :connection-manager - a connection manager to be used to speed up requests"
-  [private-key challenge response remote-ip & {:keys [ssl? proxy-host proxy-port connection-manager]
-                                               :or {ssl? false}}]
-  (if-not (and (empty? challenge)
-               (empty? response))
+      :connection-manager - a connection manager to be used to speed up requests
+      :remote-ip          - the IP address of the user who solved the CAPTCHA"
+
+  [private-key response & {:keys [proxy-host proxy-port connection-manager remote-ip]}]
+  (if-not (empty? response)
     (try
-      (let [endpoint (if ssl? https-api http-api)
+      (let [endpoint https-api
             endpoint (str endpoint "/verify")
-            resp     (client/post endpoint {:form-params        {:privatekey private-key
-                                                                 :remoteip   remote-ip
-                                                                 :challenge  challenge
-                                                                 :response   response}
-                                            :proxy-host         proxy-host
-                                            :proxy-port         proxy-port
-                                            :connection-manager connection-manager})]
+            resp (client/get endpoint {:query-params       {:secret   private-key
+                                                            :remoteip remote-ip
+                                                            :response response}
+                                       :proxy-host         proxy-host
+                                       :proxy-port         proxy-port
+                                       :connection-manager connection-manager})]
         (parse-response (:body resp)))
       (catch Exception ex
         {:valid? false :error "recaptcha-not-reachable"}))
     {:valid? false :error "incorrect-captcha-sol"}))
 
-(defn create-conn-manager
-  "Creates a reusable connection manager.
+  (defn create-conn-manager
+    "Creates a reusable connection manager.
 
-   It's just a shortcut for clj-http.conn-mgr/make-reusable-conn-manager.
-   
-   Example:
-       (create-conn-manager {:timeout 5 :threads 4})"
-  [opts]
-  (manager/make-reusable-conn-manager opts))
+     It's just a shortcut for clj-http.conn-mgr/make-reusable-conn-manager.
 
-(defn- to-json
-  "A JSON serializer for simple Clojure maps."
-  [o]
-  (cond
-   (string? o)
-   (str "'" o "'")
-
-   (keyword? o)
-   (name o)
-   
-   (map? o)
-   (let [obj-str (->>
-                  (for [[k v] o]
-                    (str (to-json k) ":" (to-json v)))
-                  (interpose ", ")
-                  (apply str))]
-     (str "{" obj-str "}"))
-
-   :default
-   o))
-
+     Example:
+         (create-conn-manager {:timeout 5 :threads 4})"
+    [opts]
+    (manager/make-reusable-conn-manager opts))
 
 (defn render
-  "Renders the HTML snippet to prompt reCAPTCHA.
+    "Renders the HTML snippet to prompt reCAPTCHA.
+    public-key - your public key (data-sitekey)
 
-  public-key - your public key
-
-  Optional parameters:
-      :error         - an error message to display (default nil)
-      :ssl?          - use HTTPS or HTTP? (default false)
-      :noscript?     - include <noscript> content (default true)
-      :display       - a map of attributes for reCAPTCHA custom theming (default nil)
-      :iframe-height - the height of noscript iframe (deafult 300)
-      :iframe-width  - the width of noscript iframe (default 500)"
-  [public-key & {:keys [error ssl? noscript? display iframe-height iframe-width]
-                 :or {ssl? false noscript? true iframe-height 300 iframe-width 500}}]
-  (let [endpoint (if ssl? https-api http-api)
-        error    (if (nil? error) "" (str "&error=" error))]
-    (str (when display
-           (str "<script type='text/javascript'>var RecaptchaOptions=" (to-json display) ";</script>"))
-         (str "<script type='text/javascript' src='" endpoint "/challenge?k=" public-key error "'></script>")
-         (when (true? noscript?) 
-           (str "<noscript>"
-                (str "<iframe src='" endpoint "/noscript?k=" public-key error "' height='" iframe-height "' width='" iframe-width "' frameborder='0'></iframe><br/>")
-                "<textarea name='recaptcha_challenge_field' rows='3' cols='40'></textarea>"
-                "<input type='hidden' name='recaptcha_response_field' value='manual_challenge'/>"
-                "</noscript>")))))
+    Optional parameters:
+        display       - a map of :symbol string attributes for reCAPTCHA custom theming (default nil)
+          :data-theme dark | light - default light
+          :data-type audio | imaage - default image
+          :data-callback javascript callback called when successful solution"
+    [public-key & {:keys [display]}]
+    (str "<script src=\"https://www.google.com/recaptcha/api.js\" async defer> </script> "
+    "<div class=\"g-recaptcha\" data-sitekey=\"" public-key "\" "
+      (clojure.string/join " " (map #(str (name (first %)) "=\"" (second %) "\"")
+                                    display)) " ></div>"))
--- test/clj_recaptcha/client_test.clj
+++ test/clj_recaptcha/client_test.clj
@@ -3,44 +3,41 @@
             [clj-recaptcha.client :as c]))
 
 (deftest verify
-  (testing "No challenge or response is given"
-    (let [res (c/verify "KEY" nil nil nil)]
-      (is (= {:valid? false :error "incorrect-captcha-sol"} res))))
+  (testing "No response is given"
+    (let [res (c/verify "KEY" nil)]
+      (is (= {:valid? false :error "incorrect-captcha-sol"} res)))))
 
   (testing "Handles exceptions"
-    (with-redefs-fn {#'clj-http.client/post (fn [url query] (throw (Exception. "Troubles!")))}
-      #(let [res (c/verify "KEY" "123" "678" "127.0.0.1")]
+    (with-redefs {#'clj-http.client/get (fn [url query] (throw (Exception. "Troubles!")))}
+      #(let [res (c/verify "KEY" "678")]
          (is (= {:valid? false :error "recaptcha-not-reachable"} res)))))
   
   (testing "Successful case"
-    (with-redefs-fn {#'clj-http.client/post (fn [url query]
-                                              (when (and (= url "http://www.google.com/recaptcha/api/verify")
-                                                         (= query {:form-params        {:privatekey "KEY"
-                                                                                        :remoteip   "127.0.0.1"
-                                                                                        :challenge  "123"
+    (with-redefs {#'clj-http.client/get (fn [url query]
+                                              (when (and (= url "https://www.google.com/recaptcha/api/verify")
+                                                         (= query {:query-params        {:secret "KEY"
                                                                                         :response   "678"}
                                                                    :proxy-host         nil
                                                                    :proxy-port         nil
                                                                    :connection-manager nil}))
                                                 {:status 200
-                                                 :body   "true\n"}))}
-      #(let [res (c/verify "KEY" "123" "678" "127.0.0.1")]
+                                                 :body   "{\n\"success\": true,\n\"error-codes\": []\n}"}))}
+      #(let [res (c/verify "KEY" "678")]
          (is (= {:valid? true :error nil} res)))))
   
   (testing "Incorrect user's input"
-    (with-redefs-fn {#'clj-http.client/post (fn [url query]
-                                              (when (and (= url "http://www.google.com/recaptcha/api/verify")
-                                                         (= query {:form-params        {:privatekey "KEY"
-                                                                                        :remoteip   "127.0.0.1"
-                                                                                        :challenge  "123"
+    (with-redefs-fn {#'clj-http.client/get (fn [url query]
+                                              (when (and (= url "https://www.google.com/recaptcha/api/verify")
+                                                         (= query {:query-params        {:secret "KEY"
+                                                                                         :remoteip nil
                                                                                         :response   "678"}
                                                                    :proxy-host         "localhost"
                                                                    :proxy-port         456
                                                                    :connection-manager :CM}))
                                                 {:status 200
-                                                 :body   "false\nsome-error-code"}))}
-      #(let [res (c/verify "KEY" "123" "678" "127.0.0.1" :proxy-host "localhost" :proxy-port 456 :connection-manager :CM)]
-         (is (= {:valid? false :error "some-error-code"} res))))))
+                                                 :body   "{\n\"success\": false,\n\"error-codes\": [\"abc\" \"def\"]\n}"}))}
+      #(let [res (c/verify "KEY" "678"  :proxy-host "localhost" :proxy-port 456 :connection-manager :CM)]
+         (is (= {:valid? false :error ["abc" "def"]} res)))))
 
 (deftest create-conn-manager-test
   (with-redefs-fn {#'clj-http.conn-mgr/make-reusable-conn-manager (fn [opts]
@@ -51,23 +48,11 @@
 (deftest render-test
   (testing "Render with default parameters"
     (let [res (c/render "234KEY")]
-      (is (true? (.contains res "http://www.google.com/recaptcha/api/noscript?k=234KEY")))
-      (is (true? (.contains res "http://www.google.com/recaptcha/api/challenge?k=234KEY")))))
+      (is (true? (.contains res "<div class=\"g-recaptcha\" data-sitekey=\"234KEY\""))))))
 
-  (testing "Render with ssl and no noscript section"
-    (let [res (c/render "234KEY" :ssl? true :noscript? false)]
-      (is (false? (.contains res "http://www.google.com/recaptcha/api/noscript?k=234KEY")))
-      (is (true? (.contains res "https://www.google.com/recaptcha/api/challenge?k=234KEY")))))
-
-  (testing "Render with error and no noscript section"
-    (let [res (c/render "234KEY" :error "recaptcha-not-reachable" :noscript? false)]
-      (is (false? (.contains res "http://www.google.com/recaptcha/api/noscript?k=234KEY")))
-      (is (true? (.contains res "http://www.google.com/recaptcha/api/challenge?k=234KEY&error=recaptcha-not-reachable")))))
 
   (testing "Render with theming"
-    (let [res (c/render "234KEY" :display {:theme "clean" :lang "de"} :iframe-height 111 :iframe-width 222)]
-      (is (true? (.contains res "RecaptchaOptions")))
-      (is (true? (.contains res "height='111'")))
-      (is (true? (.contains res "width='222'")))      
-      (is (true? (.contains res "http://www.google.com/recaptcha/api/noscript?k=234KEY")))
-      (is (true? (.contains res "http://www.google.com/recaptcha/api/challenge?k=234KEY"))))))
+    (let [res (c/render "234KEY" :display {:theme "dark" :data-type "audio" :data-callback "This is my callback"})]
+      (is (true? (.contains res "theme=\"dark\"")))
+      (is (true? (.contains res "data-type=\"audio\"")))
+      (is (true? (.contains res "data-callback=\"This is my callback\"")))))
